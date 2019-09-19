e62f61657d7d32dddc25f98b89b9ff75a2e7c77f
mrewers <mrewers@users.noreply.github.com>
2019-09-19 17:20:28 -0400

    Update changelog and versioning script

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
-const chalk = require('chalk');
-const git = require( 'simple-git' );
-const gitRev = require( 'git-rev-sync' );
-const replace = require( 'replace' );
+const chalk = require("chalk");
+const git = require("simple-git");
+const gitRev = require("git-rev-sync");
+const replace = require("replace");
 
 module.exports.versionBump = () => {
   // Get the type of version bump (major, minor, patch) from command argument
@@ -12,78 +12,74 @@ module.exports.versionBump = () => {
   const isDirty = gitRev.isDirty();
 
   // Check for uncommitted changes and throw error if present
-  if ( isDirty ) {
+  if (isDirty) {
     console.log(
-      chalk.red( 'ERROR:' ),
-      'You have uncommitted changes on your branch.',
-      '\nPlease commit all changes before attempting to version your plugin.\n'
+      chalk.red("ERROR:"),
+      "You have uncommitted changes on your branch.",
+      "\nPlease commit all changes before attempting to version your plugin.\n"
     );
     return;
   }
-  
+
   // Split the git version into it's component parts
-  const split = version.split( '.' );
-  let prefix = split[0].includes('v') ? 'v' : '';
-  let major = split[0].includes('v') ? split[0].slice(1) : split[0];
+  const split = version.split(".");
+  let prefix = split[0].includes("v") ? "v" : "";
+  let major = split[0].includes("v") ? split[0].slice(1) : split[0];
   let minor = split[1];
   let patch = split[2];
 
   // Update version based on the version type passed in the command
-  if ( type === 'major' ) {
-    major = Number( major ) + 1;
+  if (type === "major") {
+    major = Number(major) + 1;
     minor = 0;
     patch = 0;
-  } else if ( type === 'minor' ) {
-    minor = Number( minor ) + 1;
+  } else if (type === "minor") {
+    minor = Number(minor) + 1;
     patch = 0;
-  } else if ( type === 'patch' ) {
-    patch = Number( patch ) + 1;
+  } else if (type === "patch") {
+    patch = Number(patch) + 1;
   } else {
     console.log(
-      chalk.red( 'ERROR:' ),
-      'Please provide a valid version type as an argument.',
-      '\n(For example: `npm run version patch`)',
+      chalk.red("ERROR:"),
+      "Please provide a valid version type as an argument.",
+      "\n(For example: `npm run version patch`)",
       '\nAccepted values are "major", "minor", and "patch"',
-      '\nFor more information see https://semver.org/\n'
+      "\nFor more information see https://semver.org/\n"
     );
     return;
   }
-  const newVersion = `${prefix}${major}.${minor}.${patch}`
-  
-  console.log( 'Updating the plugin version number in the following locations:' );
-  
+  const newVersion = `${prefix}${major}.${minor}.${patch}`;
+
+  console.log("Updating the plugin version number in the following locations:");
+
   // Sets the new version in the plugin header
-  replace( {
+  replace({
     regex: `Version: ${version}`,
     replacement: `Version: ${newVersion}`,
-    paths: [
-      './iip-map.php'
-    ]
-  } );
+    paths: ["./iip-map.php"]
+  });
 
   // Sets the new version in hook registration
-  replace( {
+  replace({
     regex: `version = '${version}'`,
     replacement: `version = '${newVersion}'`,
-    paths: [
-      './includes/class-iip-map.php'
-    ]
-  } );
+    paths: ["./includes/class-iip-map.php"]
+  });
 
   // Sets the new version in the main, admin, and frontend package.json files
-  replace( {
+  replace({
     regex: `"version": "${version}"`,
     replacement: `"version": "${newVersion}"`,
     paths: [
-      './package.json',
-      './admin/js/package.json',
-      './public/js/package.json'
+      "./package.json",
+      "./admin/js/package.json",
+      "./public/package.json"
     ]
-  } );
+  });
 
-  // Commit version bump and tag the branch 
+  // Commit version bump and tag the branch
   git()
-    .add( './*' )
-    .commit( `${newVersion} Release` )
-    .addTag( newVersion );
-}
+    .add("./*")
+    .commit(`${newVersion} Release`)
+    .addTag(newVersion);
+};
